eabe3038e05db81801a8596bc02d738e30455589
Hiroki Furuichi <hirokicodes@gmail.com>
2019-05-10 15:36:55 +0900

    basic styling on bookmarks page

--- src/views/Bookmarks.vue
+++ src/views/Bookmarks.vue
@@ -1,11 +1,41 @@
 <template>
   <section class="section">
-    <div class="container has-text-centered">Bookmarks</div>
-    <div v-if="me">
-      <div
-        v-for="(hanzi, i) in me.bookmarkedHanzis"
-        :key="i"
-      >{{hanzi.simplified}}{{hanzi.definitions}}</div>
+    <div class="container">
+      <div class="columns is-mobile">
+        <div class="column is-10 is-offset-1">
+          <div class="level">
+            <div class="level-left">
+              <div class="title has-text-centered">Bookmarks</div>
+            </div>
+            <div class="level-right">
+              <span>View:</span>
+              <button class="button is-small">
+                <i class="fas fa-list"></i>
+              </button>
+              <button class="button is-small">
+                <i class="fas fa-th"></i>
+              </button>
+            </div>
+          </div>
+          <template v-if="me">
+            <div v-for="(hanzi, i) in me.bookmarkedHanzis" :key="i">
+              <div class="box">
+                <div class="media">
+                  <div class="media-left">{{hanzi.simplified}}</div>
+                  <div class="media-content">
+                    <div class="content">{{hanzi.definitions}}</div>
+                  </div>
+                  <div class="media-right">
+                    <button class="button is-small">
+                      <i class="fas fa-bookmark"></i>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </template>
+        </div>
+      </div>
     </div>
   </section>
 </template>
